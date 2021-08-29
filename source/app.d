c219ed5aa510ba1cd0f8dd61611f6fba4e67d73a
tynuk <bowsworddv@gmail.com>
2021-08-29 18:31:08 +0300

    add some methods

--- source/app.d
+++ source/app.d
@@ -9,4 +9,10 @@ void main()
 {
     auto c = new RPCConnector("http://127.0.0.1:8545");
     c.web3_clientVersion.writeln;
+    c.web3_sha3("0x68656c6c6f20776f726c64").writeln;
+    c.net_version.writeln;
+    c.net_listening.writeln;
+    c.net_peerCount.writeln;
+    c.eth_protocolVersion.writeln;
+    c.eth_getBalance("0x9de6f9355542ca6D16a70A57bE8Ecb751BfFc72c", "latest").writeln;
 }
--- source/rpcconnector.d
+++ source/rpcconnector.d
@@ -4,6 +4,14 @@ import rpc.protocol.json;
 
 interface IEthRPC {
     string web3_clientVersion();
+    string web3_sha3(string data);
+    string net_version();
+    bool net_listening();
+    int net_peerCount();
+    string  eth_protocolVersion();
+    // TODO: fix overloading
+    //   string eth_getBalance(string address, ulong blockNumber);
+    string eth_getBalance(string address, string tag);
 }
 
 alias RPCConnector = HttpJsonRpcAutoClient!IEthRPC;
