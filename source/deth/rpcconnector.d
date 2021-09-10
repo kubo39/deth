1b4b9be638304a599bdcca1ad6ace78bb0c56c81
tynuk <bowsworddv@gmail.com>
2021-09-10 14:05:25 +0300

    add method

--- source/deth/rpcconnector.d
+++ source/deth/rpcconnector.d
@@ -22,12 +22,33 @@ interface IEthRPC {
     int net_peerCount();
     string  eth_protocolVersion();
     JSONValue eth_syncing();
-    string eth_getBalance(string address, JSONValue blockNumber);
     string eth_coinbase();
     bool eth_mining();
     string eth_hashrate();
+    string eth_gasPrice();
     string[] eth_accounts();
-    string eth_sendTransaction(Transaction transaction);
+    string eth_blockNumber();
+    string eth_getBalance(string address, JSONValue blockNumber);
+    string eth_getStorageAt(string address, string pos, JSONValue blockNumber);
+    string eth_getTransactionCount(string address, JSONValue blockNumber);
+    string eth_getBlockTransactionCountByNumber(JSONValue blockNumber);
+    string eth_getUncleCountByBlockHash(string);
+    string eth_getUncleCountByBlockNumber(JSONValue blockNumber);
+    string eth_getCode(string address, JSONValue blockNumber);
+    string eth_sign(string address, string data);
+    string eth_signTransction(Transaction tx); 
+    string eth_sendTransaction(Transaction tx);
+    string eth_sendRawTransaction(string data);
+    string eth_call(Transaction tx, JSONValue blockNumber);
+    string eth_estimateGas(Transaction tx, JSONValue blockNumber);
+    // TODO make struct for block info 
+    JSONValue eth_getBlockByHash(string blockHash, bool isFull);
+    JSONValue eth_getBlockByNumber(JSONValue blockNumber, bool isFull);
+    JSONValue eth_getTransactionByHash(string hash);
+    JSONValue eth_getTransactionByBlockNumberAndIndex(
+            JSONValue blockNumber, string index
+    );
+
     JSONValue eth_getTransactionReceipt(string data);
 }
 
