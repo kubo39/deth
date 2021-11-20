89a85f232f52e5a324e8d65c9e629f66aac56e05
tynuk <bowsworddv@gmail.com>
2021-11-20 12:13:40 +0200

    add checking of array

--- examples/devtest/contracts/Test.sol
+++ examples/devtest/contracts/Test.sol
@@ -2,10 +2,12 @@
 pragma solidity >=0.7.0 <0.9.0;
 contract Test{
     mapping(address => int32) map;
-    uint[][] _b = [[1,2,3],[4,5]];
+    uint[][] _b;
 
     constructor(int32 d){
         map[msg.sender] = d;
+        _b.push([1, 2, 3]);
+        _b.push([4, 5]);
     }
     function set(int32 value)public{
         map[msg.sender] = value;
@@ -24,6 +26,12 @@ contract Test{
 
     function testB(uint[][] memory b) internal view returns(bool){
         if(b.length != _b.length) return false;
+        for (uint i = 0; i < b.length; i++){
+            if(b[i].length != _b[i].length) return false;
+            for(uint j = 0; j < b[i].length; j++){
+                if(b[i][j] != _b[i][j]) return false;
+            }
+        }
         return true;
     }
     
