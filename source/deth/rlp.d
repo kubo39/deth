61cdba793a9e30a4d12b1df1784bb69478de96a5
tynuk <bowsworddv@gmail.com>
2022-01-10 19:14:15 +0200

    fix rlp encode

--- source/deth/rlp.d
+++ source/deth/rlp.d
@@ -1,14 +1,15 @@
 module deth.rlp;
 
+import std;
+
 alias bytes = ubyte[];
 
 bytes rlpEncode(bytes[] a)
 {
     bytes answer = [];
-    answer ~= lenToRlp(a.length, 0xc0);
     foreach (item; a)
     {
-        if (item.length == 1 && item.length < 0x80)
+        if (item.length == 1 && item[0] < 0x80)
         {
             answer ~= item;
         }
@@ -17,7 +18,7 @@ bytes rlpEncode(bytes[] a)
             answer ~= lenToRlp(item.length, 0x80) ~ item;
         }
     }
-    return answer;
+    return lenToRlp(answer.length, 0xc0) ~ answer;
 }
 
 bytes lenToRlp(ulong l, ubyte o)
@@ -28,9 +29,7 @@ bytes lenToRlp(ulong l, ubyte o)
     }
     else
     {
-        ulong[1] t = [l];
-        bytes binL = cast(bytes) t[];
-        binL = binL.cutBytes;
+        bytes binL = l.nativeToBigEndian[].cutBytes;
         ubyte lenOfLen = cast(ubyte)(binL.length + o + 55);
         return lenOfLen ~ binL;
     }
@@ -55,4 +54,15 @@ unittest
     bytes b = [0, 0, 1, 2, 3];
     assert(a.cutBytes == b.cutBytes);
     assert(a.cutBytes == a.cutBytes);
+    ubyte[8] t = 1L.nativeToBigEndian;
+    writeln(cast(bytes) t[]);
+}
+
+unittest
+{
+    rlpEncode([cast(bytes) "cat", cast(bytes) "dog"]).map!q{a.to!string(16)}.join.writeln;
+    rlpEncode([
+        cast(bytes) "ccatcatcatcatcatcatcatcatcatcatcatcatcatcatcatcatcatcatcatcatcatcatat",
+        cast(bytes) "dog"
+    ]).map!"a.to!string(16)".join.writeln;
 }
