1964045efb49b529f79492489374b6b121c536dd
tynuk <bowsworddv@gmail.com>
2022-02-08 09:10:27 +0200

    add decimal converter

---
+++ source/deth/util/decimals.d
@@ -0,0 +1,36 @@
+module deth.util.decimals;
+
+import std.string : format;
+
+auto toWei(ulong decimals, T)(T value)
+{
+    import std.bigint : BigInt;
+
+    return value.BigInt * (10.BigInt ^^ decimals);
+}
+
+mixin template Converter(ulong decimal, string name)
+{
+    mixin(q{
+        auto %s(T)(T v)
+        {
+            return v.toWei!%d;
+        }
+   }.format(name, decimal));
+}
+
+mixin Converter!(18, "ether");
+mixin Converter!(9, "gwei");
+mixin Converter!(0, "wei");
+
+unittest
+{
+
+    import std.stdio;
+    import std.bigint : BigInt;
+
+    writeln("DEcimals test");
+    assert(10.gwei == "10_000_000_000".BigInt);
+    assert(10.ether == "10_000_000_000_000_000_000".BigInt);
+    "passed".writeln;
+}
