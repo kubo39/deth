2861d8518c4f852651755c9f572ece419848c5b0
tynuk <bowsworddv@gmail.com>
2022-02-19 01:41:05 +0200

    update constructor calling

--- examples/devtest/source/app.d
+++ examples/devtest/source/app.d
@@ -18,7 +18,7 @@ void main()
 
     auto host = environment.get("RPC_HOST", "127.0.0.1");
     auto conn = new RPCConnector("http://" ~ host ~ ":8545");
-    auto test = TestContract.deploy(conn, 32);
+    auto test = TestContract.deploy(conn, 32.wei);
 
     auto accounts = conn.remoteAccounts;
     test.get(accounts[0]).writeln;
