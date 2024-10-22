942c8e5bc5d9edd2d8edc7f5d76831c4f1888134
Hiroki Noda <kubo39@gmail.com>
2024-10-22 23:32:44 +0900

    WIP

--- source/deth/util/rlp.d
+++ source/deth/util/rlp.d
@@ -1,6 +1,10 @@
 module deth.util.rlp;
 
-import std;
+import std.array : replaceSlice;
+import std.bitmanip : nativeToBigEndian, read;
+import std.conv : to;
+import std.digest : toHexString;
+import std.range : empty;
 
 alias bytes = ubyte[];
 
@@ -79,7 +83,6 @@ unittest
 @("rlp encode")
 unittest
 {
-
     assert(rlpEncode([
             cast(bytes) "cat", cast(bytes) "dog", cast(bytes) "dogg\0y",
             cast(bytes) "man"
@@ -92,5 +95,146 @@ unittest
     assert(rlpEncode([cast(bytes) "cat", cast(bytes) ""]).toHexString == "C58363617480");
     bytes[] d = cast(bytes[])[[1], [2, 3, 4], [123, 255]];
     assert(rlpEncode(d).toHexString == "C80183020304827BFF");
+}
 
+///
+T rlpDecode(T)(const bytes input) @trusted
+{
+    static if (is(T == bool))
+    {
+        if (input.length != 1)
+            throw new Exception("too short input");
+        switch (input[0])
+        {
+        case 0x80:
+            return false;
+        case 0x1:
+            return true;
+        default:
+            throw new Exception("invalid bool value");
+        }
+    }
+    else static if (is(T == ubyte))
+    {
+        if (input.length != 1)
+            throw new Exception("invalid ubyte value");
+        return input[0] & 0x7F;
+    }
+    else static if (is(T == ushort) || is(T == uint) || is(T == ulong))
+    {
+        DecodedHeader decodedHeader = decodeRlpHeader(input[]);
+        assert(!decodedHeader.isList);
+        assert(decodedHeader.payloadLen <= T.sizeof);
+
+        if (decodedHeader.payloadLen == 0)
+            return 0;
+
+        const area = input[decodedHeader.offset .. decodedHeader.offset + decodedHeader.payloadLen];
+        auto buffer = new ubyte[T.sizeof];
+        buffer = buffer.replaceSlice(buffer[($ - area.length) .. $], area);
+        T n = buffer.read!T;
+        assert(buffer.empty);
+        return n;
+    }
+    else static if (is(T == string))
+    {
+        DecodedHeader decodedHeader = decodeRlpHeader(input[]);
+        assert(!decodedHeader.isList);
+        return cast(string) input[decodedHeader.offset .. decodedHeader.offset + decodedHeader.payloadLen];
+    }
+    else static if (is(T U == U[]))
+    {
+        DecodedHeader decodedHeader = decodeRlpHeader(input[]);
+        assert(decodedHeader.isList);
+
+        if (decodedHeader.payloadLen == 0)
+            return [];
+
+        U elem = rlpDecode!U(input[decodedHeader.offset .. decodedHeader.offset + decodedHeader.payloadLen]);
+        return [elem];
+    }
+    else static assert(false, "Unsupported type: " ~ T.stringof);
+}
+
+struct DecodedHeader
+{
+    size_t offset;
+    size_t payloadLen;
+    bool isList;
+}
+
+private DecodedHeader decodeRlpHeader(const bytes input) @trusted
+{
+    if (input.length == 0)
+    {
+        throw new Exception("input is null");
+    }
+
+    bool isList = false;
+    size_t offset;
+    size_t payloadLen;
+
+    ubyte prefix = input[0];
+    switch (prefix)
+    {
+    case 0: .. case 0x7F:
+        payloadLen = 1;
+        break;
+    case 0x80: .. case 0xB7:
+        offset = 1;
+        payloadLen = prefix - 0x80;
+        break;
+    case 0xB8: .. case 0xBF:
+        const lenOfStrLen = prefix - 0xB7;
+        const strLenArea = input[2 .. 2 + lenOfStrLen];
+
+        ubyte[] buffer = [0, 0, 0, 0, 0, 0, 0, 0];
+        // copy strLen to buffer.
+        buffer = buffer.replaceSlice(buffer[($ - lenOfStrLen) .. $], strLenArea);
+        const strLen = buffer.read!ulong;
+        assert(buffer.empty);
+
+        offset = 1 + lenOfStrLen;
+        payloadLen = strLen.to!size_t;
+        break;
+    case 0xC0: .. case 0xF7:
+        isList = true;
+        payloadLen = prefix - 0xC0;
+        break;
+    case 0xF8: .. case 0xFF:
+        isList = true;
+        break;
+    default:
+        assert(false, "unreachable");
+    }
+    return DecodedHeader(
+        offset: offset,
+        payloadLen: payloadLen,
+        isList: isList
+    );
+}
+
+@("rlp decode")
+unittest
+{
+    // bool
+    assert(rlpDecode!bool([0x80]) == false);
+    assert(rlpDecode!bool([0x01]) == true);
+
+    // ubyte
+    assert(rlpDecode!ubyte([0x80]) == 0);
+    assert(rlpDecode!ubyte([0x01]) == 1);
+
+    // ulong
+    assert(rlpDecode!ulong([0x80]) == 0);
+    assert(rlpDecode!ulong([0x09]) == 9);
+
+    // uint
+    assert(rlpDecode!uint([0x09]) == 9);
+
+    // string
+    assert(rlpDecode!string([0x83, 'd', 'o', 'g']) == "dog");
+
+    // ulong[]
+    assert(rlpDecode!(ulong[])([0xC0]) == []);
 }
