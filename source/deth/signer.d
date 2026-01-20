05101c2353cc089943a13544a804bfc270f7e2a3
Hiroki Noda <927749+kubo39@users.noreply.github.com>
2026-01-21 08:27:30 +0900

    Implements EIP-191: Signed Data Standard (#53)

--- source/deth/signer.d
+++ source/deth/signer.d
@@ -8,26 +8,29 @@ import deth.util.transaction;
 import deth.util.types : Address, Hash, bytes, convTo, ox;
 
 import keccak : keccak256;
-import secp256k1 : secp256k1;
+import secp256k1 : Signature, secp256k1;
 
+/// secp256k1 signer implementation.
 class Signer
 {
+    /// Create a new secp256k1 signer from a private key.
     this(string privateKey) @safe
     {
         this(privateKey.convTo!Hash);
     }
 
+    /// Ditto.
     this(Hash privateKey) @safe
     {
         keypair = new secp256k1(privateKey);
     }
 
-    Address address() @safe pure nothrow
+    Address address() @safe pure nothrow const
     {
         return keypair.address;
     }
 
-    /// Signs transaction by stored in wallet key
+    /// Signs a transaction by stored in a private key.
     /// Params:
     ///   tx =  tx to sign
     /// Returns: rlp encoded signed transaction
@@ -73,6 +76,61 @@ class Signer
         return tx.serializeToSignedRLP(signature);
     }
 
+    /// Signs a message by stored in a private key.
+    ///
+    /// EIP-191: Signed Data Standard
+    /// https://eips.ethereum.org/EIPS/eip-191
+    ///
+    /// hash = keccak256("\x19Ethereum Signed Message:\n" + len(message) + message)
+    Signature signMessage(bytes message) @safe
+    {
+        return keypair.signHash(eip191HashMessage(message));
+    }
+
+    /// Ditto.
+    Signature signMessage(string message) @trusted
+    {
+        return signMessage(cast(bytes) message);
+    }
+
 private:
     secp256k1 keypair;
+}
+
+/// Hash a message according to EIP-191.
+Hash eip191HashMessage(const bytes message) @safe
+{
+    return keccak256(eip191PrefixedMessage(message));
+}
+
+private ubyte[] eip191PrefixedMessage(const bytes message) @safe
+{
+    import std.array : appender;
+    import std.conv : to;
+
+    auto prefix = cast(const(ubyte)[]) "\x19Ethereum Signed Message:\n";
+    auto lenStr = cast(const(ubyte)[]) message.length.to!string;
+
+    auto buffer = appender!(ubyte[]);
+    buffer ~= prefix;
+    buffer ~= lenStr;
+    buffer ~= message;
+
+    return buffer[];
+}
+
+@("eip-191: signs message")
+unittest
+{
+    import secp256k1 : ecRecover;
+
+    auto message = "Some data";
+    auto signer = new Signer(
+        "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+    );
+    const address = signer.address;
+    auto sig = signer.signMessage(message);
+    auto hashedMsg = eip191PrefixedMessage(cast(bytes) message);
+    const recovered = ecRecover(sig, hashedMsg);
+    assert(recovered == address);
 }
